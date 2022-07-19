44894455678de492405c88419643dc794d17ef4a
Choco47 <kelvin.chu1122@gmail.com>
2022-07-19 11:16:30 +0800

    modified:   pages/admin/products.js 	modified:   pages/api/admin/products/[id]/index.js 	modified:   pages/api/admin/products/index.js

--- pages/api/admin/products/index.js
+++ pages/api/admin/products/index.js
@@ -8,12 +8,34 @@ const handler = async (req, res) => {
   if (!session || !session.user.isAdmin) {
     return res.status(401).send('Admin signin required');
   }
-  // const { user } = session;
+  //post => create product
+  //const { user } = session;
   if (req.method === 'GET') {
     return getHandler(req, res);
+  } else if (req.method === 'POST') {
+    return postHandler(req, res);
   } else {
     return res.status(400).send({ message: 'Method not allowed' });
   }
+};
+
+const postHandler = async (req, res) => {
+  await db.connect();
+  const newProduct = new Product({
+    name: 'sample name',
+    slug: 'sample-name-' + Math.random(),
+    image: '/images/blue.jpg',
+    price: 0,
+    category: 'sample category',
+    countInStock: 0,
+    description: 'sample description',
+    rating: 0,
+    numReviews: 0,
+  });
+  //after post save it
+  const product = await newProduct.save();
+  await db.disconnect();
+  res.send({ message: 'Product created successfully', product });
 };
 
 const getHandler = async (req, res) => {
